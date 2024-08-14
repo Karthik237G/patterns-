6d16c3866a8271dcb30a602643b5af7fa05a5777
Karthik237G <gollapallikarthik03@gmail.com>
2024-08-14 20:06:09 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,8 @@
-for i in range(n):
-  for j in range(n):
-      print("*",end="")
-    
-    print()
+#printing a box using stars
+def fun(n):
+  for i in range(n): #for rows
+      for j in range(n): #for columns 
+          print("*",end="") #end keyword will print the * in a single row 
+        
+        print() #this line take us to next line
+fun(4)
