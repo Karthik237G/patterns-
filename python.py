231804ddb664dbf7b712df90e26b7b6593a138fc
Karthik237G <gollapallikarthik03@gmail.com>
2024-08-14 23:52:29 +0530

    Update python.py

--- python.py
+++ python.py
@@ -6,3 +6,32 @@ def fun(n):
         
         print() #this line take us to next line
 fun(4)
+
+#creating a right angled triangle using the stars
+def fun(n):
+    for i in range(n+1):
+        for j in range(i):
+            print(" *", end="")
+        print()
+fun(4)
+
+#creating downward right angled triange
+def fun(n):
+    for i in range(n+1):
+        for j in range(n-i):
+            print(" *",end="")
+        print()
+
+fun(4)
+
+#creating an equilateral triangle
+def fun(n):
+  for i in range(n+1):
+    print("",end="")
+    for j in range (i):
+      print(" *",end="")
+    print()
+    
+
+
+      
