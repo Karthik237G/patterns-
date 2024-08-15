402212dc609abb38336eafd99516bd2717d2869c
Karthik237G <gollapallikarthik03@gmail.com>
2024-08-16 00:21:46 +0530

    Update python.py

--- python.py
+++ python.py
@@ -26,12 +26,25 @@ fun(4)
 
 #creating an equilateral triangle
 def fun(n):
-  for i in range(n+1):
-    print("",end="")
-    for j in range (i):
-      print(" *",end="")
-    print()
-    
+    for i in range(n):
+        for j in range(n, i, -1):
+            print(" ", end="")
+        for j in range(0,i,+1):
+            print("* ", end="")
+        print()
+fun(6)
 
+#printing hollow square
+def fun(n):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if(i==1 or i==n or j==1 or j==n):
+                print("*",end="")
+            else:
+                print(" ",end="")
+        print()
 
+fun(6)
+
+#printing H using stars
       
