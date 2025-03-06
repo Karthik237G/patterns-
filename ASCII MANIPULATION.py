4115bc7042161e4651828b9734aca719408d52bd
Karthik237G <gollapallikarthik03@gmail.com>
2025-03-06 09:15:20 +0530

    Update and rename ASCII MANIPULATION to ASCII MANIPULATION.py

---
+++ ASCII MANIPULATION.py
@@ -0,0 +1,32 @@
+#find the frequency of the words in a sentence EX: A cat is sitting on a mat
+#convert a number in different forms 1) binary to decimal to octal to hexa vice versa 
+#patterns using alphabets 
+#printing 
+def fun(n):
+  for i in range(n):
+    for j in range(i):
+      print(chr(i+65),end='')
+    print()
+fun(5)
+'''
+A
+AB
+ABC
+ABCD
+'''
+def fun(n):
+  for i in range(n):
+    for j in range(n,i,-1):
+      print(" ",end='')
+    for k in range(i+1):
+      print(chr(i+65),end='')
+    print()
+fun(5)
+'''
+     A
+    BB
+   CCC
+  DDDD
+ EEEEE
+ '''
+def fun(
